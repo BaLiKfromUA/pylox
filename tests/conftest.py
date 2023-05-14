2c26d89724d8ba976923f2a9d047144825da847f
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2023-05-14 17:01:20 +0100

    linter

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,9 @@ def go_to_tmpdir(request):
     # ensure local test created packages can be imported
     sys.path.insert(0, str(tmpdir))
     # copy test data to tmpdir
-    copy_tree(str(os.path.dirname(os.path.realpath(__file__))) + "/data", str(tmpdir))
+    copy_tree(
+        str(os.path.dirname(os.path.realpath(__file__))) + "/data", str(tmpdir)
+    )
     # chdir only for the duration of the test.
     with tmpdir.as_cwd():
         yield
