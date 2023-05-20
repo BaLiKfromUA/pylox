22b697a73ac2c162b44797b7cdc699611106c2b6
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2023-05-21 00:51:15 +0100

    add xfail logic

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -7,16 +7,24 @@ import pytest
 
 from pylox.cli import Lox
 
+EXPECTED_TO_FAIL = ["block/empty.lox", "assignment/to_this.lox"]
+
 
 def prepare_list_of_test_files():
     start_path = f"{os.path.dirname(os.path.realpath(__file__))}/data/"
-    return [
-        pytest.param(
-            str(path).replace(start_path, ""),
-            marks=getattr(pytest.mark, str(path).replace(start_path, "")),
-        )
+    tests = [
+        str(path).replace(start_path, "")
         for path in Path(start_path).rglob("*.lox")
     ]
+    tests_with_marks = [
+        pytest.param(test, marks=[getattr(pytest.mark, test)])
+        for test in tests
+    ]
+    for test in tests_with_marks:
+        if test.values[0] in EXPECTED_TO_FAIL:
+            test.marks.append(pytest.mark.xfail)
+
+    return tests_with_marks
 
 
 def parse_test_file(filename: Path) -> list[str]:
